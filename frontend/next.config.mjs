4c97f4edd7e38381a893d34299edcfd0f4eabbcd
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-09-07 04:11:20 -0700

    Updated Images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -24,6 +24,18 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'static.wikia.nocookie.net',
             },
+            {
+                protocol: 'https',
+                hostname: 'static.wikia.nocookie.net/twicenation',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.wikia.nocookie.net/atiny',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.wikia.nocookie.net/aespa',
+            },
         ],
     },
 };
