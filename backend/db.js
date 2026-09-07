643d321695da2124002b72830efdfa05de70db71
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-09-07 03:38:54 -0700

    Hopefully Images will show Properly

--- backend/db.js
+++ backend/db.js
@@ -5,21 +5,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const pool = new Pool(
-//     process.env.DATABASE_URL
-//         ? {
-//             connectionString: process.env.DATABASE_URL,
-//             ssl:
-//                 process.env.NODE_ENV ? { rejectUnauthorized: false } : false
-//         }
-//         : {
-//             user: process.env.DB_USER,
-//             host: process.env.DB_HOST,
-//             database: process.env.DB_NAME,
-//             password: process.env.DB_PASSWORD,
-//             port: process.env.DB_PORT
-//         }
-// );
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// });
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -31,10 +23,6 @@ const pool = new Pool({
 
 pool.on('connect', () => {
     console.log('Connected to PostgreSQL');
-});
-
-pool.on('error', error => {
-    console.error('Unexpected PostgreSQL error:', error);
 });
 
 export default pool;
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: '**',
+//             },
+//         ],
+//     },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
