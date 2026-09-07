271ec7d267299d95ff0282fd3b30617e1ccbf9ec
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-09-07 03:28:29 -0700

    Connect backend to Render PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -5,21 +5,29 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool(
-    process.env.DATABASE_URL
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl:
-                process.env.NODE_ENV ? { rejectUnauthorized: false } : false
-        }
-        : {
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.DB_PORT
-        }
-);
+// const pool = new Pool(
+//     process.env.DATABASE_URL
+//         ? {
+//             connectionString: process.env.DATABASE_URL,
+//             ssl:
+//                 process.env.NODE_ENV ? { rejectUnauthorized: false } : false
+//         }
+//         : {
+//             user: process.env.DB_USER,
+//             host: process.env.DB_HOST,
+//             database: process.env.DB_NAME,
+//             password: process.env.DB_PASSWORD,
+//             port: process.env.DB_PORT
+//         }
+// );
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+        process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false
+});
 
 pool.on('connect', () => {
     console.log('Connected to PostgreSQL');
